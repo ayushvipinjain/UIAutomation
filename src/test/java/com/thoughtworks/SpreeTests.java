f44a241df92d45b0b83ff0fc36ad38ebbb2dd377
Ayush Jain <jayush@thoughtworks.com>
2020-03-05 17:54:09 +0530

    Added tests for login and defined before and after methods

--- src/test/java/com/thoughtworks/SpreeTests.java
+++ src/test/java/com/thoughtworks/SpreeTests.java
@@ -1,25 +1,57 @@
 package com.thoughtworks;
 
+import com.beust.jcommander.Parameter;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class SpreeTests {
 
-    @Parameters("browser")
-    @Test(groups = "testGroup")
-    public void logintest(@Optional String browser){
+    WebDriver driver = null;
 
+    @Parameters("browser")
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(String browser ) throws InterruptedException {
+
+        if(browser.equals("chrome")){
+              WebDriverManager.chromedriver().setup();
+              driver = new ChromeDriver();
+        }
+        else{
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.navigate().to("https://spree-vapasi.herokuapp.com");
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
     }
 
-    @Parameters("browser")
     @Test
-    public void validateProductSearch(@Optional String browser){
+    public void validateLoginIsSuccessful() {
+        driver.findElement(By.id("link-to-login")).click();
+        driver.findElement(By.id("spree_user_email")).sendKeys("testuser@gmail.com");
+        driver.findElement( By.id("spree_user_password")).sendKeys("testuser");
+        driver.findElement(By.xpath("//input[@type='submit'][@value='Login']")).click();
+        Assert.assertEquals(driver.findElement(By.linkText("My Account")).isDisplayed(),true,"Verify Login is Successful");
+    }
 
+    @Test void validateLoginFailed() {
+        driver.findElement(By.id("link-to-login")).click();
+        driver.findElement(By.id("spree_user_email")).sendKeys("testuser@gmail.com");
+        driver.findElement( By.id("spree_user_password")).sendKeys("user");
+        driver.findElement(By.xpath("//input[@type='submit'][@value='Login']")).click();
+        boolean isLoginFailed = driver.findElement(By.xpath("//div[@class='alert alert-error'][text()='Invalid email or password.']")).isDisplayed();
+       Assert.assertEquals(isLoginFailed,true);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanSetup(){
+        driver.quit();
     }
 }
